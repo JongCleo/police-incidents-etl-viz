0f85abb8627d22b39d602f06b03a66f3dea447a2
Leo Kim <leonardkim96@gmail.com>
2022-09-30 17:09:21 -0400

    offset updating + update OR insert behavior

--- flows/01_ingest.py
+++ flows/01_ingest.py
@@ -1,12 +1,11 @@
 import csv
 import imp
 import os
-import sys
 
 import pandas as pd
 from prefect import flow, task
 from sodapy import Socrata
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 
 cwd = os.path.abspath(os.path.dirname(__file__))
 # ip = imp.load_source("models", os.path.join(cwd, "../models/incident_parts.py"))
@@ -14,14 +13,18 @@ cwd = os.path.abspath(os.path.dirname(__file__))
 client = Socrata("data.sfgov.org", None)
 
 
-@task
-def extract() -> pd.DataFrame:
+def get_current_offset() -> int:
     # this feels super monkey but.. it works
     with open(os.path.join(cwd, "offset.csv"), "r", encoding="utf-8") as f:
-        text = f.readline().strip()
+        offset = f.readline().strip()
+    return int(offset)
 
-    OFFSET = int(text)
-    LIMIT = 100
+
+@task
+def extract(offset: int) -> pd.DataFrame:
+
+    OFFSET = offset
+    LIMIT = 20000
     # id for the Police Dpt Incident Reports Data Set
     # https://dev.socrata.com/foundry/data.sfgov.org/wg3w-h783
     POLICE_INCIDENTS_DATA_SET_ID = "wg3w-h783"
@@ -57,7 +60,6 @@ def add_minor_major_col(data: pd.DataFrame) -> pd.DataFrame:
         & data["incident_subcategory"].apply(lambda x: str(x) in inc_subcat)
         & data["incident_description"].apply(lambda x: str(x) in inc_desc)
     )
-    print(res.head())
     return res
 
 
@@ -75,24 +77,100 @@ def make_compatible_with_sqlite(data: pd.DataFrame) -> pd.DataFrame:
 
 
 @task
-def load(data: pd.DataFrame) -> None:
+def insert_or_update(new_data: pd.DataFrame) -> None:
     dbengine = create_engine("sqlite:///demo.db")
+
+    if not (inspect(dbengine).has_table("incident_parts")):
+        new_data.to_sql("incident_parts", dbengine)
+        return
+
+    new_data.to_sql("potential_updates_table", dbengine, if_exists="replace")
+    update_query = """
+        UPDATE incident_parts as ip
+        SET resolution = ip.resolution
+        FROM potential_updates_table as put
+        WHERE ip.row_id = put.row_id
+    """
+    insert_query = """
+    INSERT INTO incident_parts (
+	row_id ,
+    incident_datetime,
+    incident_date,
+    incident_time,
+    incident_year,
+    incident_day_of_week ,
+    report_datetime,
+    incident_id ,
+    incident_number,
+    cad_number,
+    report_type_code,
+    report_type_description,
+    filed_online,
+    incident_code,
+    incident_category,
+    incident_subcategory,
+    incident_description,
+    is_minor,
+    resolution,
+    intersection,
+    cnn,
+    police_district,
+    analysis_neighborhood,
+    supervisor_district,
+    latitude,
+    longitude )
+SELECT 
+	row_id ,
+    incident_datetime,
+    incident_date,
+    incident_time,
+    incident_year,
+    incident_day_of_week ,
+    report_datetime,
+    incident_id ,
+    incident_number,
+    cad_number,
+    report_type_code,
+    report_type_description,
+    filed_online,
+    incident_code,
+    incident_category,
+    incident_subcategory,
+    incident_description,
+    is_minor,
+    resolution,
+    intersection,
+    cnn,
+    police_district,
+    analysis_neighborhood,
+    supervisor_district,
+    latitude,
+    longitude 
+FROM 	potential_updates_table
+WHERE row_id not in (select row_id from incident_parts)"""
+    with dbengine.begin() as conn:
+        conn.execute(update_query)
+        conn.execute(insert_query)
+
     # ip.IncidentParts.__table__.create(bind=engine, checkfirst=True)
-    data.to_sql("incident_parts", dbengine, if_exists="append", index=False)
 
 
 @task
-def update_offset() -> None:
-    pass
+def update_offset(offset_increment: int) -> None:
+    with open(os.path.join(cwd, "offset.csv"), "w", encoding="utf-8") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow([str(offset_increment)])
 
 
 @flow
 def ingest():
-    data = extract()
+    offset = get_current_offset()
+    data = extract(offset)
     minor_major_col_added = add_minor_major_col(data)
     invalids_removed = remove_out_of_sf(minor_major_col_added)
     compatible_data = make_compatible_with_sqlite(invalids_removed)
-    load(compatible_data)
+    insert_or_update(compatible_data)
+    update_offset(offset + len(data))
 
 
 ingest()
